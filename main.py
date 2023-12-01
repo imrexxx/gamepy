cf90e9ca9672b294c500d04298f4a923a913b9e6
Rex <107266420+imrexxx@users.noreply.github.com>
2023-12-01 22:22:32 +0700

    add user guess

--- main.py
+++ main.py
@@ -5,5 +5,7 @@ import random
 computer = random.randint(1, 10)
 print(computer)
 # get a user input
+guess = int(input("Enter your guess")
+print(guess)
 
 # check if user input same as computer random number
