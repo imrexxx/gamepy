92b1094ea59b0365be5e27ecb26c5e1f4276dcd7
Rizky Darmawan <mrizkydarmawan999@gmail.com>
2023-12-01 22:52:56 +0700

    add play again

--- main.py
+++ main.py
@@ -1,14 +1,21 @@
 import random
 # number guessing game
 
-# get a computer random number
-computer = random.randint(1, 10)
-print(computer)
-
 # get a user input
 # check if user input same as computer random number
-for _ in range(3) :
-    guess = int(input("Enter your guess : "))
-    if guess == computer :
-        print("Congratulations, you guess are correct!!!")
+
+while True :
+
+    # get a computer random number
+    computer = random.randint(1, 10)
+    print(computer)
+
+    for _ in range(3) :
+        guess = int(input("Enter your guess : "))
+        if guess == computer :
+            print("Congratulations, you guess are correct!!!")
+            break
+    
+    ask = input("Play again? (enter 'y' or 'n') :")
+    if ask != "y" :
         break
